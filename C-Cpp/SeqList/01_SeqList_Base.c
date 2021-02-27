e81d44921917fb97695cfaeb3772cfa2418b8be5
LuoYanP <76875261+LuoYanP@users.noreply.github.com>
2021-02-27 13:20:49 +0800

    Add Function

--- C-Cpp/SeqList/01_SeqList_Base.c
+++ C-Cpp/SeqList/01_SeqList_Base.c
@@ -1,22 +1,75 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAXSIZE 100
+#define MAXSIZE 1000
 
 typedef struct
 {
-	ElemType Data[MAXSIZE];
-	int Length;
+	int Data[MAXSIZE];
+	int Last;
 } SeqList;
 
 SeqList *Init()
 {
-	SeqList *Head = (SeqList *)malloc(sizeof(SeqList));
-	if (Head == NULL)
+	SeqList *S = (SeqList *)malloc(sizeof(SeqList));
+	if (S == NULL)
 	{
-		printf("Init Error!\n");
 		exit(0);
 	}
-	Head->Length = 0;
-	return Head;
+	S->Last = -1;
+	return S;
+}
+
+int SeqList_Length(SeqList *s)
+{
+	return (s->Last + 1);	
+} 
+
+void Read(SeqList *S, int ElementNumber)
+{
+	if(ElementNumber < 0 || ElementNumber > MAXSIZE - 1)
+	{
+		exit(0);
+	}
+	for(int i = 0; i < ElementNumber; i++)
+	{
+		int tmp;
+		scanf("%d",&tmp);
+		S->Data[i] = tmp;
+		S->Last++;
+	}
+}
+
+void Insert(SeqList *S, int Position, int e)
+{
+	if(Position < 0 || Position > S->Last)
+	{
+		exit(0);
+	}
+	for(int i = S->Last; i >= Position - 1; i--)
+	{
+		S->Data[i+1] = S->Data[i];
+	}
+	S->Data[Position - 1] = e;
+}
+
+void Print(SeqList *s)
+{
+	if(s->Last >= 0)
+	{
+		for(int j = 0; j <= s->Last; j++)
+		{
+			printf("%d ",s->Data[j]);
+		}
+	}
+}
+
+int main(void)
+{
+	int ElementNumber; 		//元素个数
+	SeqList *S = Init();
+	scanf("%d", &ElementNumber);
+	Read(S,ElementNumber);
+	Print(S);
+	return 0;
 }
